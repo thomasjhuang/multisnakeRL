164f6ce435f811be5fc4e355a317e7cff9607778
Thomas Huang <huang836@purdue.edu>
2019-03-15 20:51:17 -0400

    uploading experiment results

--- src/config.py
+++ src/config.py
@@ -4,6 +4,6 @@ agent             = "simplestrategies"
 filename          = "nonrlrun"
 game_hp           = HP(grid_size = 20, max_iter = 3000, discount = 0.9)
 depth             = lambda s,a : survivorDfunc(s, a , 2, 0.5)
-num_trials        = 1000
-opponents         = [SmartGreedyAgent, OpportunistAgent]
+num_trials        = 5000
+opponents         = [SmartGreedyAgent, OpportunistAgent, RandomAgent, GreedyAgent]
 comment           = ""
